5258db9ba4a3926edea63772114159571210da5e
amosoisin <23148082+amosoisin@users.noreply.github.com>
2026-08-16 20:59:33 +0900

    konomitv: 追加

--- setup.sh
+++ setup.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 
 cp -rf ./mirakurun/opt.sample ./mirakurun/opt
+cp -rf ./konomitv/config/config.yaml ./konomitv/KonomiTV/config.yaml
+cp -rf ./konomitv/config/docker-compose.yaml ./konomitv/KonomiTV/docker-compose.yaml
